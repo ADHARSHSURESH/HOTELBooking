1ec401e5a8ec588b89faec463d3c588d6c3e8f84
PON ADHARSH SR <adharshsr2003@gmail.com>
2026-06-18 11:13:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hotel Booking Backend Running Successfully");
+});
 
 /* ================= DATABASE ================= */
 
